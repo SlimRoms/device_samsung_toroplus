2ed1ef474b358373068331d7f05c7c9f1896a8e0
kufikugel <kufikugel@googlemail.com>
2012-12-03 06:14:50 +0100

    toroplus touches 4.2.1
    config for toroplus 4.2.1
    
    PatchSet 2
    
    cleanup tabs and whitespaces

---
+++ slim.mk
@@ -0,0 +1,42 @@
+# Boot animation
+TARGET_BOOTANIMATION_NAME := vertical-720x1280
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/toroplus/full_toroplus.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Inherit device settings
+$(call inherit-product, vendor/slim/config/common_nexus.mk)
+
+# Inherit torch settings
+#$(call inherit-product, vendor/slim/config/common_ledflash.mk)
+
+PRODUCT_COPY_FILES +=  \
+    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
+#copy 00check
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check
+
+#copy vold.fstab
+PRODUCT_COPY_FILES += \
+    device/samsung/toroplus/vold.fstab:system/etc/vold.fstab
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.camera.res=5MP
+
+# Release name
+PRODUCT_RELEASE_NAME := toroplus
+
+PRODUCT_BUILD_PROP_OVERRIDES +=  PRODUCT_NAME=mysidspr BUILD_FINGERPRINT="samsung/mysidspr/toroplus:4.2.1/JOP40D/eng.ar.20121202.005722:userdebug/release-keys"
+PRIVATE_BUILD_DESC="mysidspr-userdebug 4.2.1 JOP40D 005722 release-keys"
+
+PRODUCT_NAME := slim_toroplus
+PRODUCT_DEVICE := toroplus
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
